82c3b852b18bb97335f429d9783a99371afa6779
Pradeep_Chinthala <ch.pradeep26@gmail.com>
2018-09-28 16:59:13 +0530

    Commit

---
+++ Automation/Parameters/Parameter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace Automation.Paramters
+{
+    public class Parameter
+    {
+        public void CollectParamter(params string[] readCollection)
+        {
+            string filePath = string.Empty;
+            string directory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory + @"Parameters\");
+            if (readCollection.Length == 0)
+            {
+                filePath = directory+ "Parameter_Global.xml";
+            }
+            
+            string path = Environment.CurrentDirectory;
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(filePath);
+        }
+    }
+}
